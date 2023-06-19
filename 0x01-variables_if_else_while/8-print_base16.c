382b4344ea83f000c7c038ac6c03965cede18e25
steveorji <orjistephen79@gmail.com>
2023-06-19 13:45:49 -0700

    8-print_base16.c)

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - Prog entry point
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10, i++)
+	{
+		putchar('0' + i);
+	}
+	for  (i = 0; i = 6; i++)
+	{
+		putchar('a' + i);
+	}
+	putchar('\n');
+	return (0);
+}
