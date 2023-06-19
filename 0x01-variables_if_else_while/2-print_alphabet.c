4def2336bb4828a719eaee76a5e2d84bb303a1c3
steveorji <orjistephen79@gmail.com>
2023-06-19 05:00:30 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,10 @@ int main(void)
 {
 	char c;
 
-	c == 'a';
-	while
-		c <= 'z'; {
-			putchar(c);
-			c++;
-		}
-	putchar("\n")
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
 	return (0);
 }
